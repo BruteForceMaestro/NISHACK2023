3fa48c25029bf0e00e1915684c149d63b326cf55
KhDan007 <kh.dan007@gmail.com>
2023-11-10 20:19:13 +0600

    setup environment

--- backend/server.ts
+++ backend/server.ts
@@ -6,6 +6,7 @@ const app = express()
 // returns list of all rows basically in the users table
 app.get('/api', async (request, response) => {
     getUsers(1).then(rowlist => {
+        console.log(rowlist);
         response.status(200).json(rowlist)
     }).catch(err => {
         console.log(err)
