290962f53794f9b9b80ff87a15a4d720036ec75a
BruteForceMaestro <dimas.tabayev@gmail.com>
2023-11-11 19:40:36 +0600

    added working authentication and other api reqs

--- backend/models/user.ts
+++ backend/models/user.ts
@@ -1,3 +1,4 @@
+// internal user stuff, should never be leaked in a get request.
 export class User {
     username: string
     first_name: string
@@ -17,4 +18,29 @@ export class User {
         this.profession_id = userObject.profession_id
     }
 
+}
+
+// information that should be available to the public.
+export class PublicUser {
+    username: string
+    first_name: string
+    last_name: string
+    profession_id: number
+
+    constructor(user: User){
+        this.username = user.username
+        this.first_name = user.first_name
+        this.last_name = user.last_name
+        this.profession_id = user.profession_id
+
+    }
+}
+// for logins and such. never shared in get requests.
+export class UserLoginDetails {
+    username: string
+    password: string
+    constructor(name: string, pass: string) {
+        this.username = name
+        this.password = pass
+    }
 }
--- backend/server.ts
+++ backend/server.ts
@@ -1,14 +1,46 @@
-import { getUsers, insertUser } from "./users"
-import { User } from "./models/user"
+import { deleteUser, getUserByUsername, getUsers, insertUser } from "./users"
+import { User, PublicUser, UserLoginDetails } from "./models/user"
 import express from 'express'
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+import dotenv from 'dotenv'
+import e from "express"
+
+dotenv.config()
 const app = express()
 
 app.use(express.json())
 
-// returns list of all rows basically in the users table
+// checks body of request for type
+function typeCheck<T>(body: Object){
+    let instanceOfT : T | undefined = undefined;
+    try {
+        instanceOfT = body as T
+    }
+    catch {
+        console.log("Malformed request recieved.")
+        return undefined
+    }
+
+    return instanceOfT;
+}
+
+// sends in json so tests don't fail and shit
+function sendShortMessage(response, msg: string, statusCode: number = 400){
+    response.status(statusCode).send({message: msg})
+}
+
+// returns public knowledge about users
 app.get('/api/users', async (request, response) => {
     getUsers(1).then(rowlist => {
-        response.status(200).send(rowlist)
+        
+        let publicInfo : PublicUser[] = [];
+        rowlist.forEach(user => {
+            publicInfo.push(new PublicUser(user))
+        });
+
+        response.status(200).send(publicInfo)
+
     }).catch(err => {
         console.log(err)
         response.sendStatus(501)
@@ -18,7 +50,26 @@ app.get('/api/users', async (request, response) => {
 app.post('/api/users', async (request, response) => {
     
     response.set('Content-Type', 'application/json')
-    insertUser(request.body as User).then( us =>{
+
+    const newUser = typeCheck<User>(request.body)
+
+    if (newUser == undefined){
+        sendShortMessage(response, "Improper request")
+        return
+    }
+
+    let hashedPassword;
+    try {
+        hashedPassword = await bcrypt.hash(newUser.password, 10)
+        newUser.password = hashedPassword
+
+    } catch (err) {
+        console.error("Failed to hash password!")
+        console.error(err)
+        response.status(501).send()
+    }
+    
+    insertUser(newUser).then( us =>{
         response.status(200).send(us[0])
     }  
     ).catch(err => {
@@ -26,5 +77,109 @@ app.post('/api/users', async (request, response) => {
         response.sendStatus(501)
     })
 })
+
+app.post('/api/users/login', async (request, response) => {
+    
+    response.set('Content-Type', 'application/json')
+
+    const loginDetails = typeCheck<UserLoginDetails>(request.body)
+
+    if (loginDetails == undefined){
+        sendShortMessage(response, "Improper request")
+        return
+    }
+
+    const userRowList = await getUserByUsername(loginDetails.username);
+    const user = userRowList[0]
+
+    if (user == undefined){
+        sendShortMessage(response, `No such user found with username: ${loginDetails.username}`)
+        return
+    }
+
+    try {
+        console.log(loginDetails.password)
+        console.log(user.password)
+        if (await bcrypt.compare(loginDetails.password, user.password)){
+            console.log("Login success.")
+
+            let accessTokenSecret : string = "";
+            if (process.env.ACCESS_TOKEN_SECRET != undefined){
+                accessTokenSecret = process.env.ACCESS_TOKEN_SECRET
+            } else {
+                console.log("JWT error: env ACCESS TOKEN is undefined")
+                sendShortMessage(response, "Error occured while logging in.")
+                return
+            }
+
+            // if you get error here add ACCESS_TOKEN_SECRET to your .env
+            const accessToken = jwt.sign(user, accessTokenSecret)
+            response.status(200).send( { accessToken: accessToken } )
+        } else {
+            sendShortMessage(response, "Error occured while logging in.")
+        }
+
+    } catch (err) {
+        console.error({message: "Caught exception while login:"})
+        console.error(err)
+        sendShortMessage(response, "Error occured while logging in.")
+    }
+})
+
+// section --------authenticated user stuff
+app.get('/api/users/:username', authenticateToken, async (request, response) => {
+    let caller_user = request["user"] as User
+    if (caller_user.username != request.params.username){
+        const UserRowList = await getUserByUsername(request.params.username)
+        const returned_user = UserRowList[0]
+        response.status(200).send(new PublicUser(returned_user))
+        return
+    }
+    response.status(200).send(caller_user)
+})
+
+
+app.get('/api/users/:username/delete', authenticateToken, async (request, response) => {
+    
+    let user = request["user"] as User
+    if (user.username != request.params.username){
+        console.log("Unauthorized deletion tried.")
+        sendShortMessage(response, "Unauthorized", 401)
+        return
+    }
+    deleteUser(user).then( _ => {
+        response.status(200).send(user)
+    }).catch(err => {
+        console.error("Error while deleting a user:")
+        console.error(err)
+        sendShortMessage(response, "Internal Error", 501)
+    })
+})
+
+function authenticateToken(req, res, next) {
+    const bearer = req.headers['authorization']
+    const token = bearer && bearer.split(' ')[1]
+
+    if (token == null){
+        return res.sendStatus(401) // no access
+    }
+
+    let accessTokenSecret : string = "";
+    if (process.env.ACCESS_TOKEN_SECRET != undefined){
+        accessTokenSecret = process.env.ACCESS_TOKEN_SECRET
+    } else {
+        console.log("JWT error: env ACCESS TOKEN is undefined")
+        sendShortMessage(res, "Error occured while logging in.")
+        return
+    }
+
+    jwt.verify(token, accessTokenSecret, (err, user) => {
+        if (err) {
+            return res.sendStatus(403)
+        }
+        req["user"] = user
+        next()
+    })
+}
 
 export default app
