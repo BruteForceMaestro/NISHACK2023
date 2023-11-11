78cc2a3436795fb5f3af6ae6d40d8aeb53420650
KhDan007 <kh.dan007@gmail.com>
2023-11-11 22:46:51 +0600

    fix and prepare front to connection to backend

--- backend/result_handler.js
+++ backend/result_handler.js
@@ -1,2 +1,3 @@
 const subject = ["Kazakh A: SL", "English A: SL", "Russian A: SL", "Russian A: HL", "English B: HL", "Economics: SL", "Economics: HL", "Geography: SL", "Geography: HL", "History: SL", "History: HL", "Digital Society: SL", "Digital Society: HL", "Social and cultural anthropology: SL", "Social and cultural anthropology: HL", "Business management: HL", "Biology: SL", "Biology: HL", "Chemistry: SL", "Chemistry: HL", "Computer Science: SL", "Physics: SL", "Physics: HL", "Mathematics: AA SL", "Mathematics: AA HL", "Mathematics: AI SL", "Mathematics: AI HL", "Visual Arts: SL", "Visual Arts: HL"];
 
+
