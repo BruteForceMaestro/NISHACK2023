e12d0cdaab9ade90b52230bbbc3e4cd4b3e2084d
KhDan007 <kh.dan007@gmail.com>
2023-11-12 06:04:47 +0600

    format dp.js file

---
+++ js/dp.js
@@ -0,0 +1,201 @@
+const jobs = {
+    'Software Developer':
+        [
+            'Computer Science HL',
+            'Mathematics AA HL',
+            'Physics HL',
+            'Digital Societies SL',
+            'Design Technology SL',
+            'Business and Management SL'
+        ],
+
+    'Medical Doctor':
+        [
+            'Biology HL',
+            'Chemistry HL',
+            'Physics SL',
+            'Mathematics AA SL',
+            'Psychology SL',
+            'English Language and Literature SL'
+        ],
+
+    'Data Scientist':
+        [
+            'Mathematics HL',
+            'Computer Science HL',
+            'Physics SL',
+            'Mathematical AA HL',
+            'Digital Societies SL',
+            'Business and Management SL'
+        ],
+
+    'Financial Analyst':
+        [
+            'Mathematics AI HL',
+            'Business and Management HL',
+            'Economics HL',
+            'English B HL',
+            'Computer Science SL',
+            ' Digital Societiy SL'
+        ],
+
+    'Marketing Manager':
+        [
+            'Business and Management HL',
+            'Economics SL',
+            'Mathematics AA SL',
+            'English B HL',
+            'Visual Arts SL',
+            'Visual Arts'
+        ],
+
+    'Civil Engineer':
+        [
+            'Physics',
+            'Computer science',
+            'Mathematics AA',
+            'English B',
+            'Digital societies',
+            'Design Technology'
+        ],
+
+    'Cybersecurity Specialist':
+        [
+            'Computer Science HL',
+            'Mathematics SL',
+            'Physics SL',
+            'Digital Societies HL',
+            'Business and Management SL',
+            'English B HL'
+        ],
+
+    'Pharmacist':
+        [
+            'Biology HL',
+            'Chemistry HL',
+            'Mathematics AA SL',
+            'English B HL',
+            'Business and Management SL',
+            'Visual Arts SL'
+        ],
+
+    'Attorney':
+        [
+            'English Language and Literature HL',
+            'History HL',
+            'Business and Management SL',
+            'Economics SL',
+            'Psychology SL',
+            'English B HL'
+        ],
+
+    'Environmental Scientist':
+        [
+            'Biology HL',
+            'Chemistry SL',
+            'Environmental Systems and Societies HL',
+            'Geography SL',
+            'Mathematics SL',
+            'Visual Arts SL'
+        ],
+
+    'Surgeon':
+        [
+            'Biology HL',
+            'Chemistry HL',
+            'Physics SL',
+            'Mathematics AA SL',
+            'English B HL',
+            'Visual Arts SL'
+        ],
+
+    'Human Resources Manager':
+        [
+            'Business and Management HL',
+            'Psychology SL',
+            'Economics SL',
+            'English B HL',
+            'Mathematics AA SL',
+            'Global Politics SL'
+        ],
+
+    'Mechanical Engineer':
+        [
+            'Physics HL',
+            'Mathematics AA HL',
+            'Chemistry SL',
+            'Design Technology SL',
+            'Business and Management SL',
+            'English B SL'
+        ],
+
+    'Registered Nurse':
+        [
+            'Biology HL',
+            'Chemistry SL',
+            'Psychology SL',
+            'Mathematical AA SL',
+            'English B HL',
+            'Sports exercise and health Science HL'
+        ],
+
+    'Psychiatrist':
+        [
+            'Biology HL',
+            'Psychology HL',
+            'Chemistry SL',
+            'Mathematics AA SL',
+            'English B HL',
+            'Global Politics SL'
+        ],
+
+    'Teacher':
+        [
+            'English Language and Literature HL',
+            'Psychology SL',
+            'Business and Management SL',
+            'English B HL',
+            'Mathematics AA HL',
+            'Visual Arts SL'
+        ],
+
+    'Graphic Designer':
+        [
+            'Visual Arts HL',
+            'Design Technology HL',
+            'Computer Science HL',
+            'English B SL',
+            'Mathematical AA SL',
+            'Media SL'
+        ],
+
+    'Director':
+        [
+            'Film HL',
+            'Theatre HL',
+            'Business and Management SL',
+            'English Language and Literature HL',
+            'Global Politics SL',
+            'Visual Arts SL'
+        ],
+
+    'Actor':
+        [
+            'Theatre HL',
+            'English Language and Literature HL',
+            'Visual Arts SL',
+            'Psychology SL',
+            'Global Politics SL',
+            'Film HL'
+        ]
+    ,
+    'Biomedical Researcher':
+        [
+            'Biology HL',
+            'Chemistry HL',
+            'Physics SL',
+            'Mathematics AA SL',
+            'Psychology SL',
+            'Environmental Systems and Societies SL'
+        ]
+};
